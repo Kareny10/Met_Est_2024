251a18ece4ffecf2be188edc1a6e9fbc9b689087
Kareny10 <andreakarenyperez41635@gmail.com>
2024-12-04 21:05:04 -0600

    Add files via upload

---
+++ PIA.R
@@ -0,0 +1,55 @@
+#Andrea Kareny Pérez Pineda
+#Producto Integrador de Aprendizaje
+#Métodos Estadísticos
+#Desarrollo del curso
+
+#Desarollo del curso de Métodos Estadísticos 2024
+
+#Listado de actividades del Curso
+
+#Clase de introducción:Se dió una introcucción al 
+#curso de métodos estadisticos.
+
+#Clase 1: 20.08.2024; se ingresaror datos en los que 
+#se calculó el bloxplot, en el que se aprecian los 
+#reportes de CONAFOR. Se vio un primer contacto con el programa.
+
+#Tarea 1: 20.08.2024; Se calcularon los datos de cuánto gasta un
+#estudiante de MCF.En esta clase se vio un primer contacto con el
+#programa
+
+#Clase 2: 27.08.2024; en la clase se observo como insertar datos por
+#medio de un link.
+
+#Tarea 2: 30.08.2024, se conocieron y se utilizaroncomandos como 
+#DBH.media, mean.
+
+#Práctica: 09.09.2024;se midieron semillas en el laboratorio de Madera, 
+#de dichas semillas se tomaron los datos de diámetros y pesos.
+
+#Clase 3: 10.09.2024, se importaron datos de GitHub y se trabajó con
+#dichos datos.
+
+#Clase 4:24.09.2024; se comenzó a utilizar la prueba de t (t.test), también 
+#realizó la comparación de una muestra experimental y una muestra teorética.
+
+#Examen 1:25.09.2024, se realizó el examen correspondiente a la primera parte.
+
+#Clase: 07.10.2024; se compararon datos la fobia que tienen las personas a las
+#arañas, en dicha actividad se compararon ambos datos para saber que grupo de 
+#personas presenta una mayor ansiedad al estar cerca de estos bichos; en dicha
+#actividad se utilizaron los códigos de taply, abline y se realizó la prueba de t.
+
+
+#Clase 5: 22.10.2024; se comenzó a utilizar cádigos para poder clacular la 
+#correlación de un grupo de datos.
+
+#Clase 6:28.10.2024; se calculó la regreción lineal de un grupo de datos.
+
+#Preexamen: 29.10.2024, se realizó un preexamen para el segundo examen.
+
+#Examen 2: 11.11.2024, se realizó un examen correspondiente a la segunda parte.
+
+#Clase 7: 12.11.2024, en esta clase se utilizó ANOVA para un 
+#experimento de cuatro parajes y de sus diámetros, esto en el estado
+#de Chihuahua
